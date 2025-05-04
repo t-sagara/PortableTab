579c584895e809d5705dadc50d06d692a61b98bd
Takeshi Sagara <sagara@info-proto.com>
2025-05-04 01:04:02 +0000

    Security update.

--- PortableTab/__init__.py
+++ PortableTab/__init__.py
@@ -1,8 +1,10 @@
+from importlib.metadata import version
+
 from .base_table import BaseTable
 from .capnp_manager import CapnpManager
 from .capnp_table import CapnpTable
 
-__version__ = '0.3.6'
+__version__ = version("PortableTab")
 
 __all__ = [
     "BaseTable",
